b09c6ff1fedf216991dbe59e157a4c189fd2c014
Timmy00274672 <Timmy00274672@gmail.com>
2014-11-04 11:59:48 +0800

    Try @RunWith(Suite.class).

---
+++ src/main/java/com/diplab/timmy00274672/LearnJunit4/App.java
@@ -0,0 +1,13 @@
+package com.diplab.timmy00274672.LearnJunit4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/diplab/timmy00274672/LearnJunit4/suit/FeatureTestSuite.java
@@ -0,0 +1,16 @@
+package com.diplab.timmy00274672.LearnJunit4.suit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.diplab.timmy00274672.LearnJunit4.AppTest;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  AppTest.class, CopyOfAppTest.class
+})
+public class FeatureTestSuite {
+  // the class remains empty,
+  // used only as a holder for the above annotations
+}
